aed3a66ed90a942198b74e4f7aa8bfff91d5dc86
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2015-10-31 13:19:04 +0900

    [master] home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "homes#index"
-  resources :homes
+  root to: "home#index"
+  resources :links
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
